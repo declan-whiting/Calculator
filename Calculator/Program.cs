005b79d1b606c4d9e4db611b5efe697ab5046f53
declan-whiting <declan-whiting@outlook.com>
2020-05-04 18:44:10 +0100

    Added subtract service

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Calculator.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Calculator
@@ -11,6 +12,7 @@ namespace Calculator
 
             var serviceProvider = new ServiceCollection()
             .AddTransient<IAddService, AddService>()
+            .AddTransient<ISubtractService, SubtractService>()
             .AddSingleton<ICalculator, Calculator>()
             .BuildServiceProvider();
 
